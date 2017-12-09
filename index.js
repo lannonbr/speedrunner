c993cec5747b76a7afadd4f405e962b74c787102
Benjamin Lannon <lannonbr@clarkson.edu>
2017-12-08 22:49:08 -0500

    Reformatted code using Prettier and added WR holder name and time achieved.

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const fetch = require('node-fetch');
-const Alexa = require('alexa-sdk');
+const fetch = require("node-fetch");
+const Alexa = require("alexa-sdk");
+const moment = require("moment");
 
 var handlers = {
-    'AnyWorldRecord': function () {
-		var that = this;
-		fetch('https://www.speedrun.com/api/v1/games/76r55vd8/records')
-			.then(res => res.json())
-			.then(data => {
-				let anyWR = data.data[0].runs[0].run;
+  AnyWorldRecord: function() {
+    var that = this;
+    fetch("https://www.speedrun.com/api/v1/games/76r55vd8/records")
+      .then(res => res.json())
+      .then(data => {
+        let anyWR = data.data[0].runs[0].run;
+        let date = anyWR.date;
+        let player = anyWR.players[0].name;
+        let time = anyWR.times.primary;
 
-				let time = anyWR.times.primary;
+        let timeMatches = time.match(/PT(\d*)H(\d*)M(\d*)S/);
 
-				let matches = time.match(/PT(\d*)H(\d*)M(\d*)S/)
+        let dateObj = moment(date);
 
-				const hour = matches[1];
-				const minute = matches[2];
-				const second = matches[3];
+        const hour = timeMatches[1];
+        const minute = timeMatches[2];
+        const second = timeMatches[3];
 
-				that.emit(':tell', `The current Any % world record for Super Mario Odyssey is ${hour} Hour${hour != 1 ? 's': ''}, ${minute} minute${minute != 1 ? 's': ''}, and ${second} second${second != 1 ? 's' : ''}.`)
-			});
-    }
+        that.emit(
+          ":tell",
+          `The current Any % world record for Super Mario Odyssey is ${
+            hour
+          } Hour${hour != 1 ? "s" : ""}, ${minute} minute${
+            minute != 1 ? "s" : ""
+          }, and ${second} second${
+            second != 1 ? "s" : ""
+          }. It was achieved by ${player} on ${dateObj.format("LL")}`
+        );
+      });
+  }
 };
 
-exports.handler = function(event, context, callback){
-	var alexa = Alexa.handler(event, context, callback);
-	alexa.registerHandlers(handlers);
-	alexa.execute();
+exports.handler = function(event, context, callback) {
+  var alexa = Alexa.handler(event, context, callback);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
